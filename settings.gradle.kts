d26578b28e916c319dcef09dac63084336690025
AbdullahM0hamed <25087116+AbdullahM0hamed@users.noreply.github.com>
2022-08-15 21:14:44 +0100

    Potentially helpful comment

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -20,3 +20,7 @@ File(rootDir, ".").eachDir { dir ->
 fun File.eachDir(block: (File) -> Unit) {
     listFiles()?.filter { it.isDirectory }?.forEach { block(it) }
 }
+
+
+// To only include a single project, comment out the previous lines (except the first one), and include your plugin like so:
+// include("PluginName")
